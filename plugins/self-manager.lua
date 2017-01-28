4054af23c87a690a0641f03838d32f4cc0022f3a
Makan <makan.j5050@gmail.com>
2017-01-29 00:43:41 +0330

    Update all plugins and bot.lua

--- plugins/self-manager.lua
+++ plugins/self-manager.lua
@@ -1,19 +1,53 @@
-local function get_variables_hash(msg)
-  if gp_type(msg.chat_id_) == 'chat' or gp_type(msg.chat_id_) == 'channel' then
-    return 'chat:bot:variables'
-  end
-end 
+local function show_bot_settings(msg)
+   local text = '*》Self Bot Settings :《*\n'
 
-local function get_value(msg, var_name)
-  local hash = get_variables_hash(msg)
-  if hash then
-    local value = redis:hget(hash, var_name)
-    if not value then
-      return
+   local hash = 'autoleave'
+    if not redis:get(hash) then
+        autoleave = '[Enable]'
     else
-      return value
+        autoleave = '[Disable]'
     end
-  end
+
+   local hash = 'anti-flood'
+    if not redis:get(hash) then
+        antiflood = '[Enable]'
+    else
+        antiflood = '[Disable]'
+    end
+
+   local hash = 'markread'
+    if redis:get(hash) == "on" then
+        markread = '[Yes]'
+    else
+        markread = '[No]'
+    end
+
+   local hash = 'flood_max'
+    if not redis:get(hash) then
+        MSG_NUM_MAX = 5
+    else
+        MSG_NUM_MAX = tonumber(redis:get(hash))
+    end
+
+    local hash = 'flood_time'
+    if not redis:get(hash) then
+        TIME_CHECK = 2
+    else
+        TIME_CHECK = tonumber(redis:get(hash))
+    end
+    local hash = 'mute_gp:'..msg.chat_id_
+    if redis:get(hash) then
+        muteall = '[Enable]'
+    else
+        muteall = '[Disable]'
+    end
+if gp_type(msg.chat_id_) == 'channel' then
+    text = text..'_》Auto Leave :_ *'..autoleave..'*\n_》Mute All :_ *'..muteall..'*\n_》Messages Read :_ *'..markread..'*\n_》Pv Max Flood :_ *['..MSG_NUM_MAX..']*\n_》Pv Flood Time Check :_ *['..TIME_CHECK..']*\n_》Pv Flood Protection :_ *'..antiflood..'*\n*》*@BeyondTeam*《*'
+return text
+elseif gp_type(msg.chat_id_) == 'pv' or gp_type(msg.chat_id_) == 'chat' then
+    text = text..'_》Auto Leave :_ *'..autoleave..'*\n_》Messages Read :_ *'..markread..'*\n_》Pv Max Flood :_ *['..MSG_NUM_MAX..']*\n_》Pv Flood Time Check :_ *['..TIME_CHECK..']*\n_》Pv Flood Protection :_ *'..antiflood..'*\n*》*@BeyondTeam*《*'
+return text
+   end
 end
 
 local function enable_chat(chat_id)
@@ -34,7 +68,8 @@ local function disable_chat( chat_id )
 end
 
 local function pre_process(msg)
-	msg.text = msg.content_.text_
+local chat_id = msg.chat_id_
+local user_id = msg.sender_user_id_
 local function check_invite(arg, data)
 local hash = 'autoleave' 
 if tonumber(data.id_) == our_id and not redis:get(hash) then
@@ -48,6 +83,68 @@ end
       	user_id_ = msg.adduser
     	}, check_invite, {chat_id=msg.chat_id_,msg_id=msg.id_})
 	end
+    local hash = 'flood_max'
+    if not redis:get(hash) then
+        MSG_NUM_MAX = 5
+    else
+        MSG_NUM_MAX = tonumber(redis:get(hash))
+    end
+
+    local hash = 'flood_time'
+    if not redis:get(hash) then
+        TIME_CHECK = 2
+    else
+        TIME_CHECK = tonumber(redis:get(hash))
+    end
+    if gp_type(chat_id) == 'pv' then
+        --Checking flood
+        local hashse = 'anti-flood'
+        if not redis:get(hashse) then
+            print('anti-flood enabled')
+            -- Check flood
+                if not is_sudo(msg) then
+                    -- Increase the number of messages from the user on the chat
+                    local hash = 'flood:'..user_id..':msg-number'
+                    local msgs = tonumber(redis:get(hash) or 0)
+                    if msgs > MSG_NUM_MAX then
+          local flooder = 'flooder'
+          local is_offender = redis:sismember(flooder, user_id)
+        if is_offender then
+if redis:get('user:'..user_id..':flooder') then
+return
+else
+  tdcli.sendMessage(chat_id, msg.id_, 0, "_You are_ *blocked* _because of_ *flooding...!*", 0, "md")
+    tdcli.blockUser(user_id, dl_cb, nil)
+  local function flooder_cb(arg, data)
+   if data.username_ then
+    user_name = "@"..data.username_
+       else
+    user_name = data.first_name_
+   end
+   tdcli.sendMessage(our_id, 0, 1, 'User [ '..user_name..' ] '..data.id_..' has been blocked because of flooding!', 1)
+    end
+tdcli_function ({
+    ID = "GetUser",
+    user_id_ = user_id
+  }, flooder_cb, {chat_id=chat_id})
+redis:setex('user:'..user_id..':flooder', 15, true)
+redis:srem(flooder, user_id)
+                        end
+                    end
+        if not is_offender then
+if redis:get('user:'..user_id..':flooder') then
+return
+else
+  tdcli.sendMessage(chat_id, msg.id_, 0, "_Don't_ *flooding*, _Next time you will be_ *block...!*", 0, "md")
+redis:setex('user:'..user_id..':flooder', 2, true)
+redis:sadd(flooder, user_id)
+                          end
+                       end
+                    end
+                    redis:setex(hash, TIME_CHECK, msgs+1)
+              end
+        end
+   end
    if redis:get("mute_gp:"..msg.chat_id_) and not is_sudo(msg) then
    del_msg(msg.chat_id_, msg.id_)
   end
@@ -93,6 +190,46 @@ local hash = 'autoleave'
          end
       end
    end
+
+     if matches[1] == 'antiflood' and is_sudo(msg) then
+local hash = 'anti-flood'
+--Enable Anti-flood
+     if matches[2] == 'on' then
+  if not redis:get(hash) then
+    return '_Private_ *flood protection* _is already_ *enabled*'
+    else
+    redis:del(hash)
+   return reply_msg(msg.id, '_Private_ *flood protection* _has been_ *enabled*', ok_cb, false)
+      end
+--Disable Anti-flood
+     elseif matches[2] == 'off' then
+  if redis:get(hash) then
+    return '_Private_ *flood protection* _is already_ *disabled*'
+    else
+    redis:set(hash, true)
+   return '_Private_ *flood protection* _has been_ *disabled*'
+                   end
+             end
+       end
+                if matches[1] == 'pvfloodtime' and is_sudo(msg) then
+                    if not matches[2] then
+                    else
+                        hash = 'flood_time'
+                        redis:set(hash, matches[2])
+            return '_Private_ *flood check time* _has been set to :_ *'..matches[2]..'*'
+                    end
+          elseif matches[1] == 'pvsetflood' and is_sudo(msg) then
+                    if not matches[2] then
+                    else
+                        hash = 'flood_max'
+                        redis:set(hash, matches[2])
+            return '_Private_ *flood sensitivity* _has been set to :_ *'..matches[2]..'*'
+                    end
+                 end
+
+       if matches[1] == 'settings' and is_sudo(msg) then
+      return show_bot_settings(msg)
+                 end
 
 if matches[1] == 'help' and is_sudo(msg) then
 
@@ -208,13 +345,14 @@ return {
 		"/channel enable: enable current channel",
 		"/channel disable: disable current channel" },
 	patterns = {
+     "^[!/#](antiflood) (.*)$",
+     "^[!/#](pvfloodtime) (%d+)$",
+     "^[!/#](pvsetflood) (%d+)$",
 		"^[!/#](autoleave) (.*)$",
 		"^[!/#](settings)$",
 		"^[!/#](help)$",
 		"^[!/][Ss]elf (on)",
 		"^[!/][Ss]elf (off)" }, 
 	run = run,
-	--privileged = true,
-	--moderated = true,
 	pre_process = pre_process
 }
---
+++ plugins/self.lua
@@ -0,0 +1,168 @@
+--[[
+################################
+#                              #
+#                     Self Plugin           #
+#                              #
+#                              #
+#          by @SoLiD ⇨Saeid⇦    #
+#                              #
+#                              #
+#   Team Channel @BeyondTeam   #
+#	                           #
+#                              #
+#     Update: 23 January 2017    #
+#                              #
+#                              #
+#    Special Thx To  @Exacute for idea    #
+#                              #
+################################
+]]
+do
+local function self_names( name )
+  for k,v in pairs(_self.names) do
+    if string.lower(name) == v then
+      return k
+    end
+  end
+  -- If not found
+  return false
+end
+
+local function self_answers( answer )
+  for k,v in pairs(_self.answers) do
+    if answer == v then
+      return k
+    end
+  end
+  -- If not found
+  return false
+end
+
+local function reload_plugins( ) 
+  plugins = {} 
+  load_plugins() 
+end
+
+local function namelist(msg)
+local namelist = _self.names
+local text = "*Names list :*\n"
+for i=1,#namelist do
+    text = text..i.." - "..namelist[i].."\n"
+end
+return text
+end
+
+local function answerlist(msg)
+local answerlist = _self.answers
+local text = "*Answers list :*\n"
+for i=1,#answerlist do
+    text = text..i.." - "..answerlist[i].."\n"
+end
+return text
+end
+
+local function set_name( name )
+  -- Check if name founded
+  if self_names(name) then
+    return '_Name_ *'..name..'* _founded_'
+  end
+    -- Add to the self table
+    table.insert(_self.names, name)
+    save_self()
+    reload_plugins( )
+    -- Reload the plugins
+    return '_New name_ *'..name..'* _added to name list_'
+end
+
+local function set_answer( answer )
+  -- Check if name founded
+  if self_answers(answer) then
+    return '_Word_ *'..answer..'* _founded_'
+  end
+    -- Add to the self table
+    table.insert(_self.answers, answer)
+    save_self()
+    reload_plugins( )
+    -- Reload the plugins
+    return '_New word_ *'..answer..'* _added to answer list_'
+end
+
+local function rem_name( name )
+  local k = self_names(name)
+  -- Check if name not founded
+  if not k then
+    return '_Name_ *'..name..'* _not founded_'
+  end
+  -- remove and reload
+  table.remove(_self.names, k)
+  save_self( )
+  reload_plugins(true)
+  return '_Name_ *'..name..'* _removed from name list_'
+end
+
+local function rem_answer( answer )
+  local k = self_answers(answer)
+  -- Check if answer not founded
+  if not k then
+    return '_Word_ *'..answer..'* _not founded_'
+  end
+  -- remove and reload
+  table.remove(_self.answers, k)
+  save_self( )
+  reload_plugins(true)
+  return '_Word_ *'..answer..'* _removed from answer list_'
+end
+
+local function run(msg, matches)
+local answer = _self.answers
+local text = answer[math.random(#answer)]
+
+			if matches[1]:lower() == "addname" and is_sudo(msg) then
+      local name = matches[2]
+      return set_name(name)
+      elseif matches[1]:lower() == "remname" and is_sudo(msg) then
+      local name = matches[2]
+      return rem_name(name)
+			elseif matches[1]:lower() == "setanswer" and is_sudo(msg) then
+      local answer = matches[2]
+      return set_answer(answer)
+      elseif matches[1]:lower() == "remanswer" and is_sudo(msg) then
+      local answer = matches[2]
+      return rem_answer(answer)
+         elseif matches[1]:lower() == 'namelist' and is_sudo(msg) then
+  return tdcli.sendMessage(msg.chat_id_, msg.id_, 0, namelist(msg), 0, "md")
+         elseif matches[1]:lower() == 'answerlist' and is_sudo(msg) then
+  return tdcli.sendMessage(msg.chat_id_, msg.id_, 0, answerlist(msg), 0, "md")
+    end
+if self_names(matches[1]) then
+ local chat = tostring(msg.chat_id_)
+     if chat:match("-100") then
+    gpid = string.gsub(msg.chat_id_, "-100", "")
+     elseif chat:match("-") then
+    gpid = string.gsub(msg.chat_id_, "-", "")
+   end
+ local hash = 'on-off:'..gpid
+    if not is_sudo(msg) then
+   if redis:get(hash) then
+  return nil
+     elseif not redis:get(hash) then
+  return tdcli.sendMessage(msg.chat_id_, msg.id_, 0, text, 0, "md")
+         end
+      end
+   end 
+end
+return {
+patterns = {
+"^[!/]([Aa]ddname) (.*)$",
+"^[!/]([Rr]emname) (.*)$",
+"^[!/]([Nn]amelist)$",
+"^[!/]([Ss]etanswer) (.*)$",
+"^[!/]([Rr]emanswer) (.*)$",
+"^[!/]([Aa]nswerlist)$",
+"^(.*)$"
+},
+run = run
+}
+
+end
+--End self.lua By @SoLiD
